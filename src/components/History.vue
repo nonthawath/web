1d8285dc070917ff163e321a81055ffc4b2f8392
wannit <wannit.b@ku.th>
2020-11-27 23:34:31 +0700

    add logic for history

--- src/components/History.vue
+++ src/components/History.vue
@@ -8,37 +8,53 @@
         <v-container>
           <v-treeview rounded hoverable activatable :items="items" class="text-sm-left">
           </v-treeview>
+
         </v-container>
       </v-row>
     </v-card>
+    <!-- <v-btn @click="fetchItem">test</v-btn> -->
   </v-container>
 </template>
 
 <script>
+import moment from 'moment'
+
 export default {
-  data: () => ({
-    items: [
-      {
-        id: 1,
-        name: "วันนี้ 9/9/2009 :",
-        children: [
-          { id: 2, name: "ยืม : 10:20:70", children: [
-            { id: 1, name: "ชื่อ ฟหกด่าสว" },
-            { id: 2, name: "รหัสนิสิต : 1212312121" },
-          ]},
-        ],
-      },
-       {
-        id: 2,
-        name: "วันนี้ 10/9/2009 :",
-        children: [
-          { id: 2, name: "ยืม : 10:20:70", children: [
-            { id: 1, name: "ชื่อ ฟหกด่าสว" },
-            { id: 2, name: "รหัสนิสิต : 1212312121" },
-          ]},
-        ],
+  name: 'VueBarcodeTest',
+  data () {
+    return {
+      items:[]
+    }
+  },
+  methods: {
+    logIt (data) {
+      console.log('detected', data)
+    }
+
+  },
+  async created() {
+      var ListItem = [];
+      let result = await this.axios.get('http://127.0.0.1:3000/inventory/')
+      for(let i = 0;i<result.data.length;i++){
+        ListItem = []
+       for(let j = 0;j<result.data[i].ListItem.length;j++){
+         console.log((result.data[i].ListItem[j].itemName))
+         let datas = {
+           id :result.data[i].ListItem[j].itemid,
+           name :"Item name : "+result.data[i].ListItem[j].itemName
+         }
+        ListItem.push(datas)
+        console.log(ListItem);
+       }
+        let data = {
+          id :Number( result.data[i].Boxid),
+          name :'วันที่ : ' + moment(result.data[i].createAt).format('DD-MM-YYYY'),
+          children : ListItem
+        }
+        this.items.push(data)
       }
-    ],
-  }),
-};
+      
+      return result.data
+  },
+}
 </script>
